232cc58db48cecd503ba85b3ad535a28a03d3a2c
Ricardo <ricardo.macias1755@alumnos.udg.mx>
2025-02-23 20:48:34 -0600

    feat: agregar actividad_13

---
+++ Practicas/Actividad_13/index.php
@@ -0,0 +1,13 @@
+
+
+<html>
+    <head>
+        <title>Actividad 12</title>
+    </head>
+    <body>
+        <form name="forma1">
+            <select name="table">
+            </select>
+        </form>
+    </body>
+</html>
