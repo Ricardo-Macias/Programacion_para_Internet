83ce9e9ab02092d1fa1dd14ffb4cbac6bb6f15d3
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2025-02-23 21:42:05 -0600

    fix: corregir variable password y mostrar en ventana las variables txt_gender y txt_career

--- Practicas/Actividad_12/recibe.php
+++ Practicas/Actividad_12/recibe.php
@@ -2,23 +2,23 @@
     $name = $_POST['nombre'];
     $email = $_POST['correo'];
     $gender = $_POST['sexo'];
-    $bulletin = $_POST['boletin'];
+    $bulletin = "Acepto"; //$_POST['boletin'];
     $comment = $_POST['comentario'];
     $career = $_POST['carrera'];
-    $password = $_POST['password'];
+    $password = $_POST['pasw'];
     $average = $_POST['promedio'];
     $date = $_POST['fecha'];
 
-    $txt_gender = ($gender == 'F') ? "Femenino" : "MAsculino";
+    $txt_gender = ($gender == 'F') ? "Femenino" : "Masculino";
     $txt_career = ($career == '1') ? "Ing. Informatica" : "Ing. Computacion";
     
 
     echo "Bienvenido $name<br>";
     echo "Correo: $email<br>";
     echo "Password: $password<br>";
-    echo "Sexo: $gender<br>";
-    echo "Carrera: $career<br>";
+    echo "Sexo: $txt_gender<br>";
+    echo "Carrera: $txt_career<br>";
     echo "Promedio: $average / Fecha: $date<br>";
     echo "Comentario: $comment<br><br>";
-    echo "$bulletin acepto a recibir boletin"
+    echo "$bulletin recibir boletin"
 ?>
