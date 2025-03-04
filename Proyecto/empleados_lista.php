4cd0b1ecb20eaf671605a6c80041ef1b6b7a71e4
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2025-03-04 15:48:11 -0600

    refactor: cambiar archivo css a style_table.css

--- Proyecto/empleados_lista.php
+++ Proyecto/empleados_lista.php
@@ -9,7 +9,10 @@
 
 <html>
     <head>
-        <link href="css/table.css" rel="stylesheet" type="text/css" >
+        <!-- ICONOS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <!-- CSS -->
+        <link href="css/style_table.css" rel="stylesheet" type="text/css" >
     </head>
     <body>
         <div class="section_title">
@@ -18,7 +21,6 @@
             ?>
         </div>
         <div class="table">
-            <table>
                 <?php
                     while ($row = $res->fetch_array()){
                         echo "<div class=\"row\">";
@@ -27,14 +29,16 @@
                         $mail = $row["correo"];
                         $role = $row["rol"];
 
-                        echo "<div class=\"column\">$id</div>";
-                        echo "<div class=\"column\">$name</div>";
-                        echo "<div class=\"column\">$mail</div>";
-                        echo "<div class=\"column\">$role</div>";
+                        echo "<div class=\"column id\">$id</div>";
+                        echo "<div class=\"column name\">$name</div>";
+                        echo "<div class=\"column email\">$mail</div>";
+                        echo "<div class=\"column role\">$role</div>";
+                        echo "<div class=\"column details\"><a href=\"\"><i class=\"fa-solid fa-circle-info\"></i></a></div>";
+                        echo "<div class=\"column edit\"><a href=\"\"><i class=\"fa-solid fa-pen-to-square\"></i></a></div>";
+                        echo "<div class=\"column delete\"><a href=\"\"><i class=\"fa-solid fa-trash\"></i></a></div>";
                         echo "</div>";
                     }
                 ?>
-            </table>
         </div>
     </body>
 </html>
