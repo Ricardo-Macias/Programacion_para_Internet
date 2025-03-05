984b1b3b0ae8ca4c81ead06a7872518e59bd3bd4
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2025-03-04 21:30:08 -0600

    refactor: agregar encabezado de la tabla

--- Proyecto/empleados_lista.php
+++ Proyecto/empleados_lista.php
@@ -11,16 +11,32 @@
     <head>
         <!-- ICONOS -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        
         <!-- CSS -->
         <link href="css/style_table.css" rel="stylesheet" type="text/css" >
     </head>
     <body>
-        <div class="section_title">
-            <?php
-                echo "Listado de empleados ($num)";
-            ?>
+        <div class="head">
+            <div class="section_title">
+                <?php
+                    echo "Listado de empleados ($num)";
+                ?>
+            </div>
         </div>
-        <div class="table">
+        <div class="content">
+            <div>
+                <a href="empleados_alta.php">Agregar empleado</a>
+            </div>
+            <div class="table">
+                <div class="table-header row">
+                    <div>ID</div>
+                    <div>Nombre</div>
+                    <div>Correo</div>
+                    <div>Rol</div>
+                    <div>Detalles</div>
+                    <div>Editar</div>
+                    <div>Eliminar</div>
+                </div>
                 <?php
                     while ($row = $res->fetch_array()){
                         echo "<div class=\"row\">";
@@ -33,12 +49,13 @@
                         echo "<div class=\"column name\">$name</div>";
                         echo "<div class=\"column email\">$mail</div>";
                         echo "<div class=\"column role\">$role</div>";
-                        echo "<div class=\"column details\"><a href=\"\"><i class=\"fa-solid fa-circle-info\"></i></a></div>";
+                        echo "<div class=\"column details\"><a href=\"empleados_elimina.php?id=$id\"><i class=\"fa-solid fa-circle-info\"></i></a></div>";
                         echo "<div class=\"column edit\"><a href=\"\"><i class=\"fa-solid fa-pen-to-square\"></i></a></div>";
                         echo "<div class=\"column delete\"><a href=\"\"><i class=\"fa-solid fa-trash\"></i></a></div>";
                         echo "</div>";
                     }
                 ?>
+            </div>
         </div>
     </body>
 </html>
