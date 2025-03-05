ff87962b6588f8e9f37dc5eb60387a78fc33016d
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2025-03-04 21:28:47 -0600

    feat: agregar empleados_elimina.php

---
+++ Proyecto/empleados_elimina.php
@@ -0,0 +1,12 @@
+<?php
+    //empleados_elimina.php
+    require "funciones/conecta.php";
+    $con = conecta();
+
+    $id = $_REQUEST["id"];
+
+    $sql = "UPDATE empleados SET eliminado = 1 WHERE id = $id";
+    $res = $con->query($sql);
+
+    header("Location: empleados_lista.php");
+?>
