b3f3600e884164e2aeef40a4836235965146ae2b
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2025-03-07 00:10:08 -0600

    feat: agregar validacion para el formulario de empleados

---
+++ Proyecto/js/validar.js
@@ -0,0 +1,17 @@
+function recibe() {
+    var name = document.employed.name.value;    
+    var last_name = document.employed.last_name.value;
+    var email = document.employed.email.value;
+    var password = document.employed.password.value;
+    var rol = document.employed.rol.value;
+
+    var form = [name, last_name, email, password, rol];
+
+    if (form.some(form => form == "" || form == "0")) {
+        alert("Faltan campos por llenar");
+    } else {
+        document.employed.method = 'post';
+        document.employed.action = 'empleados_alta.php';
+        document.employed.submit();
+    }
+}
