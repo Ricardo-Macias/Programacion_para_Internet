ce466d2f274a955de8417a41dfe31a4f4f134c2a
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2025-03-06 21:44:25 -0600

    refactor: agregar la clase list junto al content y vincular style.css

--- Proyecto/empleados_lista.php
+++ Proyecto/empleados_lista.php
@@ -13,7 +13,8 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
         
         <!-- CSS -->
-        <link href="css/style_table.css" rel="stylesheet" type="text/css" >
+        <link href="css/table.css" rel="stylesheet" type="text/css" >
+        <link href="css/style.css" rel="stylesheet" type="text/css">
     </head>
     <body>
         <div class="head">
@@ -23,7 +24,7 @@
                 ?>
             </div>
         </div>
-        <div class="content">
+        <div class="content list">
             <div class="bottom">
                 <a href="empleados_alta.php">Agregar empleado</a>
             </div>
