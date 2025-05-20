4f560d8262e09d422414e4d7313ad651743e8929
ricardo <ricardomaciasmuro01@gmail.com>
2025-05-20 13:30:59 -0600

    feat: agregar boton para subir imagen y guardar en la base de datos

--- Proyecto/empleados_salva.php
+++ Proyecto/empleados_salva.php
@@ -8,13 +8,25 @@
     $password = $_POST['password'];
     $rol = $_POST['rol'];
     $password_Enc = md5($password);
-    $file_name = '';
-    $file = '';
+    $file_name = $_FILES['files']['name'];
+    $file = $_FILES['files']['tmp_name'];
+
+    $array = explode(".", $file_name);
+    $len = count($array);
+    $pos = $len - 1;
+    $extension = $array[$pos];
+
+    $dir = "images/";
+
+    $nameEnc = md5_file($file);
+    $new_file = "$nameEnc.$extension";
 
     $sql = "INSERT INTO empleados (nombre, apellidos, correo, pass, rol, archivo_nombre, archivo_file)
-    VALUES ('$name', '$last_name', '$email', '$password_Enc', '$rol', '$file_name', '$file')";
+    VALUES ('$name', '$last_name', '$email', '$password_Enc', '$rol', '$file_name', '$new_file')";
 
     $res = $con->query($sql);
 
+    //copy($file, $dir.$new_file);
+    
     header("Location: empleados_lista.php");
 ?>
