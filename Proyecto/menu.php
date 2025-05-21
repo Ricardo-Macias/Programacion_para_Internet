c93d80f64c34d244003ec989d6486fd56d5a1ca7
ricardo <ricardomaciasmuro01@gmail.com>
2025-05-21 17:49:22 -0600

    feat: crear un menu

---
+++ Proyecto/menu.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    </head>
+
+    <body>
+        <table width="100%" height="50" align=center>
+            <tr>
+                <td align="center" valign="middle"><a href="#">Inicio</a></td>
+                <td align="center" valign="middle"><a href="#">Empleados</a></td>
+                <td align="center" valign="middle"><a href="#">Productos</a></td>
+                <td align="center" valign="middle"><a href="#">Promociones</a></td>
+                <td align="center" valign="middle"><a href="#">Pedidos</a></td>
+                <td align="center" valign="middle">Bienvenido USER</td>
+                <td align="center" valign="middle"><a href="#">Cerrar Sesion</a></td>
+            </tr>
+        </table>
+    </body>
+</html>
