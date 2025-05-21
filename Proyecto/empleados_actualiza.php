c0cd96d56e3c9da1aa51007edae8386f0125999d
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2025-05-21 01:34:16 -0600

    fix: corregir la sintaxis en la variable sql

--- Proyecto/empleados_actualiza.php
+++ Proyecto/empleados_actualiza.php
@@ -2,17 +2,20 @@
     require "funciones/conecta.php";
     $con = conecta();
 
+    $id = $_POST['id'];
     $name = $_POST['name'];
     $last_name  = $_POST['last_name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
     $rol = $_POST['rol'];
-    $password_Enc = md5($password);
-    $file_name = '';
-    $file = '';
+    $password_Enc = ($password != "") ? md5($password) : '';
+    $file_name = ' ';
+    $file = ' ';
 
-    $sql = "UPDATE empleados SET nombre='$name', apellidos='$last_name'
-    correo='$email', pass='$password', rol='$rol', archivo_nombre='$file_name', archivo_file='$file'";
+    $sql = "UPDATE empleados SET nombre='$name', apellidos='$last_name',
+    correo='$email', rol='$rol', archivo_nombre='$file_name', archivo_file='$file' ";
+
+    $sql = ($password_Enc) ? $sql.", pass='".$password_Enc."' WHERE id =".$id : $sql." WHERE id =".$id;
 
     $res = $con->query($sql);
 
