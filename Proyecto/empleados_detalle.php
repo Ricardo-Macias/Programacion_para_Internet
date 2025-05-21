309d4f251a4b30dc5e3ac48551223bd5ba9c7b40
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2025-05-20 23:24:08 -0600

    feat: terminar y agregar diseño a detalle

--- Proyecto/empleados_detalle.php
+++ Proyecto/empleados_detalle.php
@@ -8,28 +8,43 @@
     $res = $con->query($sql);
     $num = $res->num_rows;
 
-    $row = $con->fetch_array();
+    $row = $res->fetch_array();
 
     $name = $row['nombre'].' '.$row['apellidos'];
-    $email = $row['corro'];
+    $email = $row['correo'];
+    $rol = ($row['rol'] == 1) ? 'Gerente' : 'Ejecutivo';
+    $status = ($row['eliminado' == 0]) ? 'Activo' : 'Inactivo';
 
 ?>
 
 <html>
     <head>
+        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link href="css/detalle.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-
-        <div>
+        <div class="head">
+            <div class="section_title">
+                <?php
+                    echo "Detalle Empleado";
+                ?>
+            </div>
+            <div class="content-botton">
+                <div class="botton">
+                    <a href="empleados_lista.php">Regresar al Listado</a>
+                </div>
+            </div>
         </div>
 
-        <div>
-            <div>
+        <div class="content grid-content">
+            <div class="image">
             </div>
-            <div>
+            <div class="info">
                 <?php
-                    echo $name;
-                    echo $email;
+                    echo "<div> <b>Nombre:</b> $name </div>";
+                    echo "<div> <b>Correo:</b> $email </div>";
+                    echo "<div> <b>Rol:</b> $rol </div>";
+                    echo "<div> <b>Status:</b> $status</div>";
                 ?>
             </div>
         </div>        
