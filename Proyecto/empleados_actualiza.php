1400d0ce4b9404de5c471916af6329fbe8b4fe7e
Ricardo_Macias_Muro <ricardo.macias1755@alumnos.udg.mx>
2025-05-21 20:51:54 -0600

    feat: actualizar el archivo en la base de datos

--- Proyecto/empleados_actualiza.php
+++ Proyecto/empleados_actualiza.php
@@ -8,12 +8,26 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
     $rol = $_POST['rol'];
-    $password_Enc = ($password != "") ? md5($password) : '';
-    $file_name = ' ';
-    $file = ' ';
+    $password_Enc = ($password) ? md5($password) : '';
+    $file_name = $_FILES['files']['name'];
+    $file = $_FILES['files']['tmp_name'];
 
-    $sql = "UPDATE empleados SET nombre='$name', apellidos='$last_name',
-    correo='$email', rol='$rol', archivo_nombre='$file_name', archivo_file='$file' ";
+    $sql = "UPDATE empleados SET nombre='$name', apellidos='$last_name',correo='$email', rol='$rol'";
+
+    if($file){
+        $array = explode(".", $file_name);
+        $len = count($array);
+        $pos = $len - 1;
+        $extension = $array[$pos];
+
+        $dir = __DIR__."/images/";
+
+        $nameEnc = md5_file($file);
+        $new_file = "$nameEnc.$extension";
+        
+        $sql = $sql.", archivo_nombre='$file_name', archivo_file='$new_file' ";
+    }
+
 
     $sql = ($password_Enc) ? $sql.", pass='".$password_Enc."' WHERE id =".$id : $sql." WHERE id =".$id;
 
